90fd00046a5d21bd395d92a45c25d5b52b43c1ec
Sam Chapman <schap843@gmail.com>
2016-12-13 19:13:21 +0900

    change client socket url

--- src/app/services/chat.service.ts
+++ src/app/services/chat.service.ts
@@ -7,7 +7,7 @@ import 'rxjs/add/operator/map';
 
 @Injectable()
 export class ChatService {
-  private url = 'wss://c2b-chat.herokuapp.com:3000/';
+  private url = 'wss://c2b-chat.herokuapp.com/';
   private socket;
   public sessionId;
 
