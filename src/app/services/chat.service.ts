69df1d5a452f20d7226e8fbea1dc4bb41f2d31ec
Sam Chapman <schap843@gmail.com>
2016-12-16 10:47:26 +0900

    aws

--- src/app/services/chat.service.ts
+++ src/app/services/chat.service.ts
@@ -32,6 +32,7 @@ export class ChatService {
         this.sessionId = data;
       });
       this.socket.on('message', (data) => {
+        console.log('socket works!');
         observer.next(data);
       });
       return () => {
